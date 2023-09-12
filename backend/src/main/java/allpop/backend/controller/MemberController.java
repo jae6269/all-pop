5dd164889493d9e124b6ed9133df52a2d80fe7e0
SeungjunNA <nsjjsng1@gmail.com>
2023-09-12 23:09:14 +0900

    프로젝트 생성

--- backend/src/main/java/allpop/backend/controller/MemberController.java
+++ backend/src/main/java/allpop/backend/controller/MemberController.java
@@ -1,5 +1,7 @@
 package allpop.backend.controller;
 
+import allpop.backend.dto.MemberDto;
+import allpop.backend.entity.Member;
 import allpop.backend.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +18,8 @@ public class MemberController {
     }
 
     @PostMapping("/signup")
-    public String MemberSignup(){
-        memberService
+    public String MemberSignup(Member member){
+        memberService.signup(member);
+        return "login";
     }
 }
